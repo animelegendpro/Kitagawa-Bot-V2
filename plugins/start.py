1194ca220c11bed224c7c5b19f35035092f5eb61
ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>
2024-05-01 08:46:05 +0530

    Update start.py
    Signed-off-by: ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 # add time im seconds for waitingwaiting before delete 
 # 1 minutes = 60, 2 minutes = 60×2=120, 5 minutes = 60×5=300
-SECONDS = int(os.getenv("SECONDS", "600"))
+SECONDS = int(os.getenv("SECONDS", "86400"))
 
 
 
