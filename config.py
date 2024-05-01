b03f3fc669c26c802f67c825fc320afe458d167f
ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>
2024-05-01 08:47:23 +0530

    Update config.py
    Signed-off-by: ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
 FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
 
 
-SECONDS = int(os.getenv("SECONDS", "60000")) # auto delete in seconds
+SECONDS = int(os.getenv("SECONDS", "86400")) # auto delete in seconds
 
 
 
