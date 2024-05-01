5453b67e50b1d27d97862aaa10b3a3b47eb0437d
ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>
2024-05-01 10:15:35 +0530

    Update cbb.py
    Signed-off-by: ShadowKing50k <167697347+ShadowKing50k@users.noreply.github.com>

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>๏ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n๏ Language : <code>Python3</code>\n๏ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n๏ Source Code : <a href='https://github.com'>Click here</a>\n<b>๏ Channel :</b> <a href='https://t.me/ZPro_Bots_V1'>ZPro Bots V1</a>\n<b>๏ Support Group :</b> <a href='https://t.me/+FGM0HOnjDC45ZDk1'>Support</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
